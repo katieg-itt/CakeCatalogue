277470ffeab22519b67cde3c962efb55e025cabe
Katie Griffiths <Katie Griffiths>
2017-08-02 19:18:21 +0100

    Move BakedGoodsType into BakedGoodsDetails

--- BakedGoods.java
+++ BakedGoods.java
@@ -9,22 +9,16 @@ public class BakedGoods {
     //Parent class for cakes and cupcakes
     
     private double price;
-    private BakedGoodsType type;
     private BakedGoodsDetails details;
     
     
-    public BakedGoods(BakedGoodsType type, BakedGoodsDetails details, double price){
+    public BakedGoods(BakedGoodsDetails details, double price){
         this.price = price;
         this.details = details;
-        this.type = type;
     }
     
     public double getPrice() {
         return price;
-    }
-    
-    public BakedGoodsType getType() {
-        return type;
     }
     
     public BakedGoodsDetails getDetails(){
