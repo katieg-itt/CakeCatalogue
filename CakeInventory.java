feb82b6bddc829459fcc24689d95309a33e9547b
Katie Griffiths <Katie Griffiths>
2017-08-02 19:26:20 +0100

    Simplify CakeInventory to work with changes to BakedGoods

--- CakeInventory.java
+++ CakeInventory.java
@@ -13,44 +13,21 @@ public class CakeInventory {
     
     public CakeInventory(){
         Cakes = new LinkedList();
-        
     }
-     public void addCake(CakeIngredients ingre, int size, float price){
-        Cake cake = new Cake(ingre, size, price);
-        Cakes.add(cake);
+    
+    public void addBakedGood(BakedGoodsDetails details, float price) {
+        BakedGoods item = new BakedGoods(details, price);
+        Cakes.add(item);
     }
+    
      
-    public void addCupCake(CupcakeIngredients ingre, int boxCount, float price) {
-        Cupcake cupcake = new Cupcake(ingre, boxCount, price);
-        Cakes.add(cupcake);
-    }
-     public Cake getCake(String flavour){
-        for (Iterator i = Cakes.iterator(); i.hasNext();){
-            Cake cake = (Cake)i.next();
-            if (cake.getcakeSpec().equals(flavour)){
-                return cake;
-            }
-        }
-        return null;
-    }
-     
-     public List searchCake(Ingredients searchIngre){
+     public List searchCake(BakedGoodsDetails details){
          System.out.println("Checking Cakes");
          List compareCakes = new LinkedList();
          for(Iterator i = Cakes.iterator(); i.hasNext();){
-             Object next = i.next(); // put it in a temporary object
-             
-             if(next instanceof Cake) {
-                Cake cake = (Cake)next;
-                if(cake.getcakeSpec().compare(searchIngre)) {
-                    compareCakes.add(cake);
-                }
-             } else {
-                Cupcake cupcake = (Cupcake)next;
-                 
-                if(cupcake.getcakeSpec().compare(searchIngre)) {
-                    compareCakes.add(cupcake);
-                }
+             BakedGoods next = (BakedGoods)i.next();
+             if(details.matches(next.getDetails())) {
+                 compareCakes.add(next);
              }
          }
          return compareCakes;
